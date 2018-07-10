c5f9cc0a0c1b1f658ce03fc50f432c0caf24d9fd
feche <fecheromero@gmail.com>
2018-07-09 22:40:47 -0300

    agrega api rest

---
+++ subastasautomaticasiasc/lib/apiRest.ex
@@ -0,0 +1,81 @@
+  defmodule Router.Buyers do
+    use Maru.Router
+  
+    namespace :buyers do
+  
+      params do
+        requires :logicName, type: String
+        requires :ip, type: String
+        requires :tags, type: List[String] , keep_blank: true
+      end
+  
+      post do
+        #crear el nuevo Buyer
+        json(conn, "creado")
+      end
+    end
+  end
+  
+  defmodule Router.Bids do
+    use Maru.Router
+  
+    namespace :bids do
+  
+      params do
+        requires :price, type: Integer
+        requires :duration, type: Integer
+        requires :tags, type: List[String] , keep_blank: true
+        requires :article, type: Map, keep_blank: true
+      end
+  
+      post do
+        #crear el nuevo Bid
+        json(conn, "cread0")
+      end
+    end
+  end
+  
+  
+  defmodule Router.Homepage do
+    use Maru.Router
+  
+    resources do
+      mount Router.Buyers
+      mount Router.Bids
+    end
+  end
+  
+  
+  defmodule MyAPP.API do
+    use Maru.Router
+  
+  
+    plug Plug.Parsers,
+      pass: ["*/*"],
+      json_decoder: Jason,
+      parsers: [:urlencoded, :json, :multipart]
+  
+    mount Router.Homepage
+  
+    rescue_from Unauthorized, as: e do
+      IO.inspect e
+  
+      conn
+      |> put_status(401)
+      |> text("Unauthorized")
+    end
+  
+    rescue_from [MatchError, RuntimeError], with: :custom_error
+  
+    rescue_from :all, as: e do
+      conn
+      |> put_status(Plug.Exception.status(e))
+      |> text("Server Error")
+    end
+  
+    defp custom_error(conn, exception) do
+      conn
+      |> put_status(500)
+      |> text(exception.message)
+    end
+  end

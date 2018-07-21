ae3aa9339531283336c7d2766f30c1860202c290
Gustrucco <gus_trucco776@yahoo.com.ar>
2018-07-21 16:40:06 -0300

    creo las tablas

---
+++ subastasautomaticasiasc/lib/dataBase.ex
@@ -0,0 +1,18 @@
+defmodule DataBase do
+use GenServer
+
+  def start_link(_) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, [])
+    GenServer.call(pid, :create_tables)
+    {:ok, pid}
+  end
+
+  def handle_call(:create_tables, _from, _) do
+    :ets.new(:buyers, [:set, :public, :named_table])
+	:ets.new(:bids, [:set, :public, :named_table])
+  end
+end
+
+:ets.lookup(:buyers, "doomspork")
+
+:ets.lookup(:bids, "doomspork")

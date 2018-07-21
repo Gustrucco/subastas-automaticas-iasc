13031ff723e47a80ffb7414ce9a653f1fb5c5264
Gustrucco <gus_trucco776@yahoo.com.ar>
2018-07-21 18:11:38 -0300

    logs del BuyerNotifier.Supervisor

--- subastasautomaticasiasc/lib/buyerNotifierSupervisor.ex
+++ subastasautomaticasiasc/lib/buyerNotifierSupervisor.ex
@@ -2,10 +2,12 @@ defmodule BuyerNotifier.Supervisor do
   use Supervisor
 
 	def start_link(_arg) do
+    	IO.puts "********* START_LINK BuyerNotifier.Supervisor *********"
 		Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
 	end
 
 	def init(:ok) do
+    	IO.puts "********* INIT BuyerNotifier.Supervisor *********"
 		children = [
 			Supervisor.child_spec({BuyerNotifier, []}, id: BuyerNotifier)
 		]

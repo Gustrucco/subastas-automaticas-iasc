13761a6b74da488fff2b24e0cdb0429580dfac95
Gustrucco <gus_trucco776@yahoo.com.ar>
2018-07-23 01:17:47 -0300

    buyerNotifier funcionando

--- subastasautomaticasiasc/lib/buyerNotifier.ex
+++ subastasautomaticasiasc/lib/buyerNotifier.ex
@@ -2,27 +2,7 @@ defmodule BuyerNotifier do
 	use GenServer
 
 	def start_link(_arg) do
-		GenServer.start_link(__MODULE__, [])
-	end
-
-	def add_buyer(pid, buyer) do
-		GenServer.cast(pid, {:add_buyer, buyer})
-	end
-
-	def get_buyers(pid) do
-		GenServer.call(pid, :get_buyers)
-	end
-
-	def notify_new_bid(pid, bid) do
-		GenServer.cast(pid, {:notify_new_bid, bid})
-	end
-
-	def notify_new_price(pid, bid) do
-		GenServer.cast(pid, {:notify_new_price, bid})
-	end
-
-	def notify_cancelation(pid, bid) do
-		GenServer.cast(pid, {:notify_cancelation, bid})
+		GenServer.start_link(__MODULE__, [], name: __MODULE__)
 	end
 
 	# SERVER
@@ -31,31 +11,27 @@ defmodule BuyerNotifier do
 		{:ok, []}
 	end
 
-	def handle_cast({:add_buyer, new_buyer}, buyers) do
-		{:noreply, [new_buyer | buyers]}
+	def getBuyers do
+		List.flatten(:ets.match(:buyers, { :"_", :"$1", :"_", :"_", :"_", :"_"}))
 	end
 
-	def handle_cast({:notify_new_bid, bid}, buyers) do
-		Enum.each(buyers, fn buyer -> GenServer.cast(buyer,{:notify_new_bid, bid}) end)
-		{:noreply, buyers}
+	def handle_cast({:notify_new_bid, bid}, state) do
+		Enum.each(BuyerNotifier.getBuyers(), fn buyer -> GenServer.cast(buyer,{:notify_new_bid, bid}) end)
+		{:noreply, state}
 	end
 
-	def handle_cast({:notify_new_price, bid}, buyers) do
-		Enum.each(buyers, fn buyer -> GenServer.cast(buyer,{:notify_new_price, bid}) end)
-		{:noreply, buyers}
+	def handle_cast({:notify_new_price, bid}, state) do
+		Enum.each(BuyerNotifier.getBuyers(), fn buyer -> GenServer.cast(buyer,{:notify_new_price, bid}) end)
+		{:noreply, state}
 	end
 
-	def handle_cast({:notify_cancelation, bid}, buyers) do
-		Enum.each(buyers, fn buyer -> GenServer.cast(buyer,{:notify_cancelation, bid}) end)
-		{:noreply, buyers}
+	def handle_cast({:notify_cancelation, bid}, state) do
+		Enum.each(BuyerNotifier.getBuyers(), fn buyer -> GenServer.cast(buyer,{:notify_cancelation, bid}) end)
+		{:noreply, state}
 	end
 
-	def handle_cast({:notify_ending, bid}, buyers) do
-		Enum.each(buyers, fn buyer -> GenServer.cast(buyer,{:notify_ending, bid}) end)
-		{:noreply, buyers}
-	end
-
-	def handle_call(:get_buyers, _from, buyers) do
-		{:reply, buyers, buyers}
+	def handle_cast({:notify_ending, bid}, state) do
+		Enum.each(BuyerNotifier.getBuyers(), fn buyer -> GenServer.cast(buyer,{:notify_ending, bid}) end)
+		{:noreply, state}
 	end
 end

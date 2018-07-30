fab8d17d2141264d382891906eaad93b139f61a1
Rodolfo Leandro Caputo <roli@RolisMac.local>
2018-07-29 23:12:35 -0300

    Initializing variable outiside IF construct

--- worker/lib/Pid.ex
+++ worker/lib/Pid.ex
@@ -3,20 +3,21 @@ defmodule Pid do
     pidStr = :erlang.pid_to_list(aPid)
     pidStr1 = :lists.sublist(pidStr, 2, length(pidStr)-2)
     [a,b,c] = :string.lexemes(pidStr1,'.')
-    {a,b,c} 
+    {a,b,c}
   end
 
-  def tokens_to_pid({token1,token2,token3}) do 
+  def tokens_to_pid({token1,token2,token3}) do
   :erlang.list_to_pid('<' ++ token1 ++ '.' ++ token2 ++ '.' ++ token3 ++ '>')
   end
 
   def node_token(node) do 
+    aPid = []
     if (node != Node.self()) do
-    aPid = List.first(:rpc.call(node,Process,:list,[]))   
+    aPid = List.first(:rpc.call(node,Process,:list,[]))
     else
       IO.puts("otro Aca")
       otherNode = Enum.find(Node.list(),fn(aNode) -> aNode != node end)
-      aPid = List.first(:rpc.call(otherNode,Pid,:node_token,[node]))   
+      aPid = List.first(:rpc.call(otherNode,Pid,:node_token,[node]))
     end
     {nodeToken,_,_} = pid_tokens(aPid)
     nodeToken
@@ -26,7 +27,7 @@ defmodule Pid do
   List.first(Process.list)
   end
 
-  def your_token do 
+  def your_token do
     aPid = :rpc.call(List.first(Node.list),Pid,:a_pid,[])
     {token,_,_} = Pid.pid_tokens(aPid)
     token
